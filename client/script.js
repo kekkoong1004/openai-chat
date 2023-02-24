a810b8576a9144d25140de2df581aa336b75488a
kekkoong <kekkoong2062@gmail.com>
2023-02-24 21:34:34 +0800

    changed fetch API address

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ const handleSubmit = async e => {
   console.log(data.get('prompt'));
   // fetch data from server -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://openai-chatbox.herokuapp.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
