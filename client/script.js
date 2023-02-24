2e68e98bef5296b1ee68e74bee44ff0e91cc0249
kekkoong <kekkoong2062@gmail.com>
2023-02-24 21:38:38 +0800

    fixed bug

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ const handleSubmit = async e => {
   console.log(data.get('prompt'));
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://openai-chatbox.herokuapp.com/', {
+  const response = await fetch('https://openai-chatbox.herokuapp.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
